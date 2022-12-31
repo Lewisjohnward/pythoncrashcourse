c0e0227a0190011fc604410c31cdf2af5f33d602
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2022-12-31 10:29:01 +0000

    changed dir name

--- chapter2/variablesandsimpledatatypes.py
+++ chapter2/variablesandsimpledatatypes.py
@@ -17,10 +17,26 @@ print(full_name)
 
 # Removing leading whitespace
 funky_str = "  hello right whitespace"
+right_whitespace_str = "hello  "
 print(funky_str)
 print(funky_str.lstrip())
+print(right_whitespace_str.rstrip())
 
 # Remove whitespace from string
 another_funky_str = "h e l l o"
 print(another_funky_str)
 print(another_funky_str.replace(" ", ""))
+
+# Large numbers can be allocated using underscore to ease reading
+large_num = 8_000_000
+print(large_num)
+
+# Multiple assignments can be done in a single line
+x, y, z = 6, 7, 8
+print(f'{x}, {y}, {z}')
+
+# A constant is like a variable whose value stays the same throughout the program
+
+MAX_CONNECTIONS = 5000
+print(MAX_CONNECTIONS)
+
